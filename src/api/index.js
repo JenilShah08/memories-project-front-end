53f76b6b3d9a8cf19bd2fbbf52848302257b2d2c
Jenil Shah <85637539+JenilShah08@users.noreply.github.com>
2024-10-10 18:54:43 +0530

    changes in api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://memories-project-jade.vercel.app/post';
+const url = 'https://memories-project-jade.vercel.app';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
