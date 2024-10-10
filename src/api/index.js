99e1add7fdbf84bd1c6c2ce2af38f991405b7ead
Jenil Shah <85637539+JenilShah08@users.noreply.github.com>
2024-10-10 18:29:22 +0530

    changes in api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'memories-project-git-main-jenilshah08s-projects.vercel.app/post';
+const url = 'memories-project-git-main-jenilshah08s-projects.vercel.app/';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
